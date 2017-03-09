1498194b61ec7b572b0b6e7e973d4e4f45d35b6d
梁惠涌 <lianghuiyong@foxmail.com>
2017-03-09 23:25:22 +0800

    up

--- src/test3.go
+++ src/test3.go
@@ -1,16 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	test_array()
 	test_slice()
 }
 
 /*-----测试数组-----*/
-func test_array()  {
-	var name  = 
+func test_array() {
+	arr := []int{1, 2, 3, 4}
+	fmt.Println(arr)
+	arr[0]= 3
+	arr[1]= 3
+	arr[2]= 3
+	arr[3]= 3
+	fmt.Println(arr)
 }
 
 /*------测试切片-----*/
-func test_slice()  {
-	
+func test_slice() {
+
 }
